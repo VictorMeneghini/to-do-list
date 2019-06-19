f65aded63cdfcc48fa6d55fa3010181fc71a6d3d
VictorMeneghini <vhmeneghini@gmail.com>
2019-06-18 22:23:07 -0300

    Create task class

---
+++ to_do_list/task/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from to_do_list.status.models import Status
+
+class Board(models.Model):
+
+    name = models.CharField(
+        max_length=50,
+        blank=False
+    )
+
+    description = models.TextField(
+        null=False,
+        blank=False
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    status = models.ForeignKey(
+        Status,
+        on_delete=models.CASCADE,
+        related_name='tasks')
+
+    def __str__(self):
+        return self.name

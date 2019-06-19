052c6ca04677b8199d930e290808ea9d156c51a9
VictorMeneghini <vhmeneghini@gmail.com>
2019-06-18 22:24:14 -0300

    Add apps to settings.py

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'to_do_list.board',
+    'to_do_list.status',
+    'to_do_list.task'
 ]
 
 MIDDLEWARE = [

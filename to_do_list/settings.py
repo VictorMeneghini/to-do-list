ddd5cf7e5dd3783f89b620b3fef0767d1c5422fe
VictorMeneghini <vhmeneghini@gmail.com>
2019-06-18 23:22:59 -0300

    Add django rest framework

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'to_do_list.board',
     'to_do_list.status',
     'to_do_list.task'

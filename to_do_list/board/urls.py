4df177618f3c3f2826829adfe0e3bda66a4d1fd0
VictorMeneghini <vhmeneghini@gmail.com>
2019-06-23 18:52:10 -0300

    Fix path to actions

--- to_do_list/board/urls.py
+++ to_do_list/board/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 
-from . import views
+from .views import Index, CreateBoard, BoardDelete, BoardUpdate
 
 app_name = 'boards'
 urlpatterns = [
-    path('', login_required(views.index), name='index'),
+    path('', login_required(Index.as_view()), name='index'),
+    path('create/', login_required(CreateBoard.as_view()), name="board_create"),
+    path('update/<int:pk>', login_required(BoardUpdate.as_view()), name="board_update"),
+    path('delete/<int:pk>', BoardDelete.as_view(), name="board_delete"),
 ]

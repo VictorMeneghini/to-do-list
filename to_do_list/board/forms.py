406c19481df48d71ec58e00dd1e66b2e6a84d1aa
VictorMeneghini <vhmeneghini@gmail.com>
2019-06-23 18:52:32 -0300

    Create class form board

---
+++ to_do_list/board/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+from .models import Board
+
+
+class BoardForm(forms.ModelForm):
+    name = forms.CharField(
+        label="Board Name",
+        required=True,
+        max_length=255,
+    )
+
+    description = forms.CharField(
+        label="Description",
+        required=True,
+        max_length=255,
+    )
+
+    class Meta:
+        fields = [
+            "name",
+            "description"
+        ]
+        model = Board

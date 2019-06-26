6f6cb55e6eae6f19e64f47d3dc6fdc7dbc6152cf
VictorMeneghini <vhmeneghini@gmail.com>
2019-06-26 00:43:12 -0300

    Config all routers for tasks

---
+++ to_do_list/task/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib.auth.decorators import login_required
+
+from .views import CreateTask, UpdateTask, TaskDelete
+
+app_name = 'tasks'
+urlpatterns = [
+    path('create/<int:pk>', login_required(CreateTask.as_view()), name="task_create"),
+    path('update/<int:pk>', login_required(UpdateTask.as_view()), name="task_update"),
+    path('delete/<int:pk>', TaskDelete.as_view(), name="task_delete")
+]
